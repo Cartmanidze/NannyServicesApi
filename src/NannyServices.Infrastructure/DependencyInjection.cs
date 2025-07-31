5cbc59800dca2bf3873ec2a8949657a23ff0f746
Глеб Безруков <glebon84@gmail.com>
2025-07-31 23:40:42 +0600

    Check env

--- src/NannyServices.Infrastructure/DependencyInjection.cs
+++ src/NannyServices.Infrastructure/DependencyInjection.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using NannyServices.Domain.Repositories;
 using NannyServices.Infrastructure.Data;
 using NannyServices.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Hosting;
 
 namespace NannyServices.Infrastructure;
 
@@ -11,14 +12,17 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
+        var env = configuration[WebHostDefaults.EnvironmentKey];
+
+        var isTest = string.Equals(env, "Test", StringComparison.OrdinalIgnoreCase);
         var connectionString = configuration.GetConnectionString("DefaultConnection");
-        
-        if (!string.IsNullOrWhiteSpace(connectionString))
+
+        if (!isTest)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
         }
-
+        
         services.AddScoped<ICustomerRepository, CustomerRepository>();
         services.AddScoped<IProductRepository, ProductRepository>();
         services.AddScoped<IOrderRepository, OrderRepository>();
